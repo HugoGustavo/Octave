0a14a1df575d4404d253683783f8179063b14616
Hugo Gustavo <hugog.valin@gmail.com>
2018-09-14 19:33:35 -0300

    Data Types

---
+++ m-files.m
@@ -0,0 +1,11 @@
+NoOfStudents = 6000;
+TeachingStaff = 150;
+NonTeachingStaff = 20;
+Total = NoOfStudents + TeachingStaff + NonTeachingStaff;
+disp(Total);
+
+a=5;b=7;
+c=a+b
+d=c+sin(b)
+e=5*d
+f=exp(-d)
